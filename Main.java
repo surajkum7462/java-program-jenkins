e64adc5e095ea06f8845c27431e46bc6eaa69992
surajkumar <kumarsuraj7462998828@gmail.com>
2025-07-20 17:37:51 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main{
     System.out.println("This is java program");
     System.out.println("Output from java program");
     System.out.println("Date: "+new Date());
+    System.out.println("Initial Commit");
   }
 }
