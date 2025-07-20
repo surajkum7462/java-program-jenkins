901aacf3722d32ab97457ad1660dd6187faa8a02
surajkumar <kumarsuraj7462998828@gmail.com>
2025-07-20 20:15:07 +0530

    Third Commit

--- Main.java
+++ Main.java
@@ -7,5 +7,7 @@ public class Main{
     System.out.println("Date: "+new Date());
     System.out.println("Initial Commit");
     System.out.println("Added one more file");
+    Student st = new Student();
+    st.sayHello();
   }
 }
---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+  public Student()
+  {
+    System.out.println("Creating student of Object");
+  }
+
+  public void sayHello()
+  {
+    System.out.println("Say Hello");
+  }
+  
+}
